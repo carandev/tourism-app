e6aa1a08d70b97f542093eb2124a529f8d91b3c3
Carlos Gomez <carlosandres0741@gmail.com>
2023-01-10 15:58:35 -0500

    feat: added security CORS

--- src/main/java/me/carandev/tourismapp/controllers/CityController.java
+++ src/main/java/me/carandev/tourismapp/controllers/CityController.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import me.carandev.tourismapp.entities.City;
 import me.carandev.tourismapp.services.CityService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
 public class CityController {
   @Autowired
   CityService cityService;
--- src/main/java/me/carandev/tourismapp/controllers/TouristController.java
+++ src/main/java/me/carandev/tourismapp/controllers/TouristController.java
@@ -3,6 +3,7 @@ package me.carandev.tourismapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import me.carandev.tourismapp.entities.Tourist;
 import me.carandev.tourismapp.services.TouristService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class TouristController {
 
