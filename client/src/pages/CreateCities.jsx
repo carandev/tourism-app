20259c713226d644b65839f1bb22d0d1e3a0d88b
Carlos Gomez <carlosandres0741@gmail.com>
2023-01-11 11:06:23 -0500

    feat: added create forms

--- client/src/pages/CreateCities.jsx
+++ client/src/pages/CreateCities.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { useLocation } from 'wouter'
 
 const CreateCities = () => {
+  const [city, setCity] = React.useState({
+    name: '',
+    population: 0,
+    touristAttraction: '',
+    moreReservedHotel: ''
+  })
+  const [_, setLocation] = useLocation()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch('http://localhost:8080/cities', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(city)
+    }).then(response => response.ok && setLocation('/cities'))
+  }
+
+  const handleInputChange = event => {
+    let { name, value } = event.target
+
+    const newCity = { ...city }
+
+    if (name === 'population') value = parseInt(value)
+
+    newCity[name] = value
+
+    setCity(newCity)
+  }
+
   return (
-    <div>CreateCities</div>
+    <form onSubmit={handleSubmit}>
+      <input name="name" type="text" value={city.name} onChange={handleInputChange} />
+      <input name="population" type="number" value={city.population} onChange={handleInputChange} />
+      <input name="touristAttraction" type="text" value={city.touristAttraction} onChange={handleInputChange} />
+      <input name="moreReservedHotel" type="text" value={city.moreReservedHotel} onChange={handleInputChange} />
+      <button type="submit">Agregar</button>
+    </form>
   )
 }
 
--- client/src/pages/CreateTourists.jsx
+++ client/src/pages/CreateTourists.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { useLocation } from 'wouter'
 
 const CreateTourists = () => {
+  const [tourist, setTourist] = React.useState({
+    name: '',
+    birthDate: '',
+    identification: '',
+    identificationType: '',
+    travelFrecuency: 0,
+    travelBudget: '',
+    card: false
+  })
+  const [_, setLocation] = useLocation()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch('http://localhost:8080/tourists', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(tourist)
+    }).then(response => response.ok && setLocation('/tourists'))
+  }
+
+  const handleInputChange = event => {
+    let { name, value } = event.target
+
+    const newCity = { ...tourist }
+
+    if (name === 'travelFrecuency') value = parseInt(value)
+
+    if (name === 'travelBudget') value = parseFloat(value)
+
+    if (name === 'card') value = event.target.checked
+
+    newCity[name] = value
+
+    setTourist(newCity)
+  }
+
   return (
-    <div>CreateTourists</div>
+    <form onSubmit={handleSubmit}>
+      <input name="name" type="text" value={tourist.name} onChange={handleInputChange} />
+      <input name="birthDate" type="date" value={tourist.birthDate} onChange={handleInputChange} />
+      <input name="identification" type="text" value={tourist.identification} onChange={handleInputChange} />
+      <input name="identificationType" type="text" value={tourist.identificationType} onChange={handleInputChange} />
+      <input name="travelFrecuency" type="number" value={tourist.travelFrecuency} onChange={handleInputChange} />
+      <input name="travelBudget" type="text" value={tourist.travelBudget} onChange={handleInputChange} />
+      <input name="card" type="checkbox" onChange={handleInputChange} />
+      <button type="submit">Agregar</button>
+    </form>
   )
 }
 
